53414c40dffdb94640fd0b8b7e0e22cc87cd4335
LUCAS NICOLELI DE MELLO <lucas_n_mello@estudante.sesisenai.org.br>
2025-07-28 21:34:03 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+let usuarios = [
+    { id: 1, nome: 'Celso' },
+    { id: 2, nome: 'Maria' },
+    { id: 3, nome: 'Carlos' },
+    { id: 4, nome: 'Ana Paula' },
+]
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+  });
+
+app.get('/', (req, res) => {
+  res.send('SERVIDOR NODE JS + Express rodando');
+});
+
+app.get('/usuarios', (req, res) => {
+  res.json(usuarios);
+});
+
+app.get('/usuarios/:id', (req, res) => {
+    const id = req.params.id;
+    const usuario = usuarios.find(u => u.id === parseInt(id));
+    if (usuario) {
+        res.json(usuario);
+    } else {
+        res.status(404).send('Usuário não encontrado');
+    }
+  });
+
+  app.delete('/usuarios/:id', (req, res) => {
+
+    const id = parseInt(req.params.id);
+    usuario = usuarios.filter(u => u.id !== id)
+    res.json({message: `Usuário com id ${id} 
+        nome: ${id.nome} deletado com sucesso!`});
+
+  })
+    
+    
+
+
+
+
+
+
